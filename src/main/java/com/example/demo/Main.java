2bf81a0bf0d64977da1123d6fa26357b5b572c8e
Jerceka <a2h2m2e2d@gmail.com>
2019-12-10 17:05:18 +0200

    access DB with Links

--- src/main/java/com/example/demo/Main.java
+++ src/main/java/com/example/demo/Main.java
@@ -2,8 +2,10 @@ package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -26,5 +28,15 @@ public class Main {
 		MV.addObject(a);
 		System.out.println(repo.findByName("Merve"));
 		return MV;
+	}
+	@RequestMapping("accounts")
+	@ResponseBody
+	public String getall() {
+		return repo.findAll().toString();
+	}
+	@RequestMapping("account/{id}")
+	@ResponseBody
+	public String getid(@PathVariable("id")int a) {
+		return repo.findById(a).toString();
 	}
 }
