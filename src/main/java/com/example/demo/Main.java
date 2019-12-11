6327907d96e67a21be311592186fa3b50ff703ea
Jerceka <a2h2m2e2d@gmail.com>
2019-12-11 13:57:48 +0200

    PutMapping and DeleteMapping

--- src/main/java/com/example/demo/Main.java
+++ src/main/java/com/example/demo/Main.java
@@ -3,10 +3,13 @@ package com.example.demo;
 import java.util.List;
 import java.util.Optional;
 
+import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -46,5 +49,16 @@ public class Main {
 	public Account create(@RequestBody Account a) {
 		repo.save(a);
 		return a;
+	}
+	@DeleteMapping(path="/accounts/{id}",consumes= {"application/json"})
+	public List<Account> delete(@PathVariable int id) {
+		Account a = repo.getOne(id);
+		repo.delete(a);
+		return repo.findAll();
+	}
+	@PutMapping(path="/accounts")
+	public List<Account> update(@RequestBody Account a){
+		repo.save(a);
+		return repo.findAll();
 	}
 }
